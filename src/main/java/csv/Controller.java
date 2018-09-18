aef379aef7cbc65ecc97939c6cc7b5c8b188327c
dilanSachi <dilansachinthanos@gmail.com>
2018-09-18 19:26:26 +0530

    FXML

--- src/main/java/csv/Controller.java
+++ src/main/java/csv/Controller.java
@@ -1,8 +1,12 @@
 package csv;
 
+import java.awt.event.ActionEvent;
+
 public class Controller {
 
-
+    public void previousData(ActionEvent event){
+        System.out.println("Bleh");
+    }
 
 
 }
